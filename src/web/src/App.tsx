f2ea9ccdf168430c54c8dbba177085c338cfcc38
RobsonLang <robsondev.lang@hotmail.com>
2026-04-30 13:50:37 -0300

    docs: ajuste na documentação

---
+++ src/web/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World - React + TypeScript + Vite</h1>
+      <ButtonComponent label="Click Me"></ButtonComponent>
+    </>
+  );
+}
+
+export default App;
