76d91a21ea8bbdefb6b456f61e5c906cf57a8dc3
RobsonLang <robsondev.lang@hotmail.com>
2026-05-25 14:41:02 -0300

    refactor: Ajuste no layout dos cards dos produtos

--- src/web/src/modules/product/components/CardComponent/CardItem.tsx
+++ src/web/src/modules/product/components/CardComponent/CardItem.tsx
@@ -2,6 +2,10 @@ import { IconButton } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import type { Product } from "./types";
+import "./CardItem.css";
+import { useState } from "react";
+import ButtonComponent from "@/shared/components/ButtonComponent/ButtonComponent";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 interface CardItemProps {
   product: Product;
@@ -9,12 +13,25 @@ interface CardItemProps {
   onToggleFavorite: (id: string) => void;
   onShare: (product: Product) => void;
 }
+
 export default function CardItem({
   product,
   isFavorite,
   onToggleFavorite,
   onShare,
 }: CardItemProps) {
+  const [value, setValue] = useState<number>(0);
+
+  const decrease = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  };
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+
   return (
     <div className="card">
       <div className="card-name">
@@ -32,7 +49,26 @@ export default function CardItem({
       <img className="image" src={product.image} alt={product.name} />
       <div className="info">
         <p className="description">{product.description}</p>
-        <span className="price">{product.price}</span>
+        <div className="price-container">
+          <span className="price">R$ {product.price}</span>
+
+          <div className="actions-container">
+            <div className="counter-container">
+              <button onClick={decrease} className="counter-button">
+                −
+              </button>
+
+              <span className="counter-value">{value}</span>
+
+              <button onClick={increase} className="counter-button">
+                +
+              </button>
+            </div>
+            <ButtonComponent onClick={() => {}} className="AddCar">
+              <AddShoppingCartIcon />
+            </ButtonComponent>
+          </div>
+        </div>
       </div>
     </div>
   );
